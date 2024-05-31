ebc9f34f7b3c8e9483d6e7ce52e63d84e391943f
OsmanTunahan <osmantunahan007@gmail.com>
2024-05-31 23:53:45 +0300

    feat(auth): update jwt token expire date

--- app/auth/service.py
+++ app/auth/service.py
@@ -1,4 +1,5 @@
-import datetime, jwt
+import jwt
+import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.config import Config
 from app.auth.init.model import UserModel
@@ -9,9 +10,9 @@ class AuthService:
         user = UserModel.find_by_username(username)
         if user and check_password_hash(user.password, password):
             token = jwt.encode({
-                'user_id': user.id,
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
-            }, Config.JWT_SECRET_KEY)
+                'user_id': user['id'],
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=6)
+            }, Config.JWT_SECRET_KEY, algorithm='HS256')
             return token
         return None
 
