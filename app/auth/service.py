ffaf0e74d59ae27981a50aec7bed4e466a35b375
OsmanTunahan <osmantunahan007@gmail.com>
2024-05-31 23:50:41 +0300

    feat(auth): add services

---
+++ app/auth/service.py
@@ -0,0 +1,24 @@
+import datetime, jwt
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.config import Config
+from app.auth.init.model import UserModel
+
+class AuthService:
+    @staticmethod
+    def login(username, password):
+        user = UserModel.find_by_username(username)
+        if user and check_password_hash(user.password, password):
+            token = jwt.encode({
+                'user_id': user.id,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+            }, Config.JWT_SECRET_KEY)
+            return token
+        return None
+
+    @staticmethod
+    def register(username, password):
+        if UserModel.find_by_username(username):
+            return False
+        hashed_password = generate_password_hash(password)
+        UserModel.create_user(username, hashed_password)
+        return True
