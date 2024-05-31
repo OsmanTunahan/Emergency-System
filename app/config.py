f9d9b2b9b275eb15fe962d2ea0c1ec9e1ea8005c
OsmanTunahan <osmantunahan007@gmail.com>
2024-06-01 00:25:06 +0300

    add sql config

--- app/config.py
+++ app/config.py
@@ -3,3 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or ''
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or ''
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/emergency'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
