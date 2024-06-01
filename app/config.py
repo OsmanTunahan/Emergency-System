b47a1c679973efee975d96f892fc0d8f8196714d
OsmanTunahan <osmantunahan007@gmail.com>
2024-06-01 20:19:42 +0300

    update config

--- app/config.py
+++ app/config.py
@@ -6,5 +6,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:@localhost/emergency'
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('TRACK_MODIFICATIONS') or False
     DISCORD_WEBHOOK_URL = os.environ.get('DISCORD_WEBHOOK_URL') or ''
-    TELEGRAM_CLIENT_ID = os.environ.get('TELEGRAM_CLIENT_ID') or ''
+    TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID') or ''
     TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN') or ''
