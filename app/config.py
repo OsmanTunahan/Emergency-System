c11722dc226a026b5937ecbd1c4d15dbc97f3c91
OsmanTunahan <osmantunahan007@gmail.com>
2024-06-01 20:16:56 +0300

    update config

--- app/config.py
+++ app/config.py
@@ -3,5 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or ''
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or ''
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/emergency'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:@localhost/emergency'
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('TRACK_MODIFICATIONS') or False
+    DISCORD_WEBHOOK_URL = os.environ.get('DISCORD_WEBHOOK_URL') or ''
+    TELEGRAM_CLIENT_ID = os.environ.get('TELEGRAM_CLIENT_ID') or ''
+    TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN') or ''
